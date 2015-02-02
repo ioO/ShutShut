f203a96bfdf12072bbc8f079ff5f4b9c3785549d
Lionel Chanson <github@lionelchanson.fr>
2015-02-02 22:26:28 +0100

    try to use subprocess

--- server_test.py
+++ server_test.py
@@ -2,13 +2,18 @@ import unittest
 import requests
 import server
 import subprocess
+import os
+
+DEV_NULL = open(os.devnull, 'w')
 
 class ServerTests(unittest.TestCase):
     """ Demo how to use ShutShut with tests """
 
     def setUp(self):
         """ Run ShutShut """
-        subproccess.call(['python', 'server.py'])
+        subprocess.call(['python', 'server.py'], stderr=DEV_NULL, 
+                stdout=DEV_NULL
+                )
 
     def test_index(self):
         """ Request index.htm """

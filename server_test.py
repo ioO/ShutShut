171229e5a9f69dd2e6fd667e36fe39c2b6eeb9d0
Lionel Chanson <github@lionelchanson.fr>
2015-02-02 22:10:35 +0100

    fix method declaration

--- server_test.py
+++ server_test.py
@@ -5,11 +5,11 @@ import server
 class ServerTests(unittest.TestCase):
     """ Demo how to use ShutShut with tests """
 
-    class setUp(self):
+    def setUp(self):
         """ Run ShutShut """
         server.run()
 
-    class test_index(self):
+    def test_index(self):
         """ Request index.htm """
         r = requests.get('http://localhost:8080')
         self.assertEqual(r.status_code, 200)

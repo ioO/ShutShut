5333bec5a483e056f6d0b649e004ba18687444ba
Lionel Chanson <github@lionelchanson.fr>
2015-02-02 22:08:02 +0100

    add test index.htm

--- server_test.py
+++ server_test.py
@@ -1,10 +1,15 @@
 import unittest
 import requests
+import server
 
 class ServerTests(unittest.TestCase):
     """ Demo how to use ShutShut with tests """
 
     class setUp(self):
+        """ Run ShutShut """
+        server.run()
 
-    class tearDown(self):
-
+    class test_index(self):
+        """ Request index.htm """
+        r = requests.get('http://localhost:8080')
+        self.assertEqual(r.status_code, 200)
